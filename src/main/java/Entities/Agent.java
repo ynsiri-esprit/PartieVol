4fb0cac484826b543c943eee8d740da2709387a7
zaineb nahdi <zeinabnahdi@gmail.com>
2025-01-24 09:56:11 +0100

    test

--- src/main/java/Entities/Agent.java
+++ src/main/java/Entities/Agent.java
@@ -10,5 +10,6 @@ public class Agent extends Visiteur {
     public Agent(String cin, String nom, String prenom, Date dateNaissance, String email, String telephone, String login, String motDePasse) {
         super(cin, nom, prenom, dateNaissance, email, telephone, login, motDePasse);
     }
+    //zeineeed
 }
 
