fcd0e3ae9745e4a5b8acbd6eb66ca9ab82548a73
zaineb nahdi <zeinabnahdi@gmail.com>
2025-01-24 10:06:36 +0100

    Classe offre

---
+++ src/main/java/Entities/Offre.java
@@ -0,0 +1,63 @@
+package Entities;
+
+public class Offre {
+    private int id;
+    private String description;
+    private float tarif;
+
+    // Default constructor
+    public Offre() {
+    }
+
+    // Parameterized constructor
+    public Offre(int id, String description, float tarif) {
+        this.id = id;
+        this.description = description;
+        this.tarif = tarif;
+    }
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getTarif() {
+        return tarif;
+    }
+
+    public void setTarif(float tarif) {
+        this.tarif = tarif;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Offre{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", tarif=" + tarif +
+                '}';
+    }
+
+    // equals method
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Offre offre = (Offre) o;
+        return id == offre.id;
+    }
+}
+
