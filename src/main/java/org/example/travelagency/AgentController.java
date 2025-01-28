7f89e547ec50bcdd17bd184caac089bf8359ade4
Mohamedazizhamdi <azizhamdi475@gmail.com>
2025-01-28 19:59:49 +0100

    Ajout partie conncetion

--- src/main/java/org/example/travelagency/AgentController.java
+++ src/main/java/org/example/travelagency/AgentController.java
@@ -2,7 +2,6 @@ package org.example.travelagency;
 
 import Services.Impl.AgentServiceImpl;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
