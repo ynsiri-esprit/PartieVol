c1b1a234f09b9f61762615364f79bae5fe59091c
ynsiri-esprit <nsiriyassine0@gmail.com>
2025-02-13 21:05:26 +0100

    yassine nsiri

--- src/main/java/org/example/travelagency/HelloApplication.java
+++ src/main/java/org/example/travelagency/HelloApplication.java
@@ -4,20 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    public static void main(String[] args) {
-        launch();
-    }
-
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AideUserController.class.getResource("AideUser.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 800);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("FlightClient.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 400);
+        //stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
